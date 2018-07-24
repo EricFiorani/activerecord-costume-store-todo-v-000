55de36dcfeaaa03c6d81c60a591e34189bc6908b
Eric Fiorani <efiorani305@gmail.com>
2018-07-24 15:56:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,3 +5,10 @@ class CostumeStores < ActiveRecord::Migration
       t.string :name
       t.string :location
       t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+end
